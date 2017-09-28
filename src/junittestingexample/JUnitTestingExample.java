4d542f368df85a1fb5b55caa5f771a2c1ec3eb7b
GeVic <veekiyadav@gmail.com>
2017-09-28 09:27:19 +0530

    Changes made!

---
+++ src/junittestingexample/JUnitTestingExample.java
@@ -0,0 +1,22 @@
+
+package junittestingexample;
+
+/**
+ *
+ * @author veeki.yadav
+ */
+public class JUnitTestingExample {
+    // adding numbers 
+    public static int add(int a, int b){
+        return a+b;
+    }
+    
+    // adding string
+    public static String addString(String a, String b){
+        return a+b;
+    }
+    
+}
+
+    
+
